73afacc1a417dcdbe60d1bfc69ee76df21e85783
jaupi98 <alessio.jaupllari@maxwell.mi.it>
2023-05-17 09:47:57 +0200

    fine

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// let num_1 = '15';
+// let num_2 = '5';
+
+// let somma = num_1 + num_2;
+// console.log(somma);
+// console.log(num_1 / num_2);
+
+// let name = 'Fabrizio';
+// let surname = 'Mastrobattista';
+
+// console.log(name +' '+ surname);
+// console.log(`${name} ${surname}`);
+
+// let name = prompt('Come ti chiami?');
+// console.log(name);
+
+// document.getElementById('greetings').innerHTML = name;
+// console.log(` ${name}` );
+
+// let is_num = prompt('inserisci il numero');
+// console.log(is_num);
+
+// console.log(10 + is_num);
+
+// let eta = prompt( 'quanti anni hai?');
+// let anno_cor = 2023;
+
+// let anno_nascita = anno_cor - eta;
+
+// document.getElementById('anni').innerHTML =`sei nato nel ${anno_nascita}`;
+let name = prompt(' Quale il tuo nome?');
+
+let surname = prompt('Quale il tuo cognome?');
+
+let color = prompt('quale il tuo colore');
+
+
+document.getElementById('info').innerHTML =`${name}${surname}${color}22`;
